7688b270acd5e0e45b61022aadab6526871f58ec
Jay <jkrishnanu@gmail.com>
2021-03-29 13:09:13 -0700

    Add xcode files

---
+++ TestProject/TestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectApp.swift
+//  TestProject
+//
+//  Created by Jayakrishnan u on 3/29/21.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
